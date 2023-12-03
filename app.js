6c568f70e5edc91fb0f177f55da688068213f7c8
George Ershov <64842499+GeorgeStudio96@users.noreply.github.com>
2023-12-03 11:49:54 +0300

    lesson04

--- app.js
+++ app.js
@@ -1,15 +1,32 @@
-// titleProject (string)
-// screensValue (string)
-// screenPrice (num)
-// percentage (num)
-// fullPrice (num)
-// responsive (bool)
+// let x = 11;
+
+// if (x > 10) {
+//     console.log('Условие верно');
+// } else {
+//     console.log('Условие не верно');
+// }
 
 
-let titleProject = 'Калькулятор по разработке сайта'
-let screensValue = 'шаблонные, с уникальным дизайном, с анимациями'
-let screenPrice = 10000
-let percentage = 10
-let fullPrice = 100000
-let responsive = true
+
+// let a = 5;
+// let b = '10'
+// let res = a + +b
+// console.log(a.toString());
+
+// console.log(res);
+
+// console.log(typeof a);
+// console.log(typeof b);
+
+// console.log(a+b);
+
+// alert('Привет')
+
+let age = +prompt('Укажите Ваш возраст') // 15 = 25
+let a = 10
+let res = age + a
+
+console.log(res);
+
+
 
